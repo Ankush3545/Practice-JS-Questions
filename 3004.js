6d87073b33d9bc8b227786e3930bb3e39a56282d
Ankush Patrawat <165781306+Ankush3545@users.noreply.github.com>
2025-05-02 20:36:48 +0530

    Add files via upload

---
+++ 3004.js
@@ -0,0 +1,11 @@
+// program of swapping of two numbers
+var a=parseInt(prompt("Enter the number a:"));
+var b=parseInt(prompt("Enter the number b:"));
+var temp;
+console.log(`Before Swapping: value of a is: ${a}  value of b is: ${b}`);
+temp=a;
+a=b;
+b=temp;
+console.log(`After swapping: value of a is :${a}  value of b is :${b}`);
+
+

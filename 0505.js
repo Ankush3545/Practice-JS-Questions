20bc681f26dea259f3ef93ff2ab9188bd4ec7088
Ankush Patrawat <165781306+Ankush3545@users.noreply.github.com>
2025-05-09 20:40:51 +0530

    Add files via upload

---
+++ 0505.js
@@ -0,0 +1,33 @@
+// program to find largest among 3 numbers
+/*function checklargest(a,b,c){
+    if(a>=b && a>=c){
+        return a;
+    }
+    else if(b>=a && b>=c){
+        return b;
+    }
+    else{
+        return c;
+    }
+}*/
+let a= parseInt(prompt("Enter the value a"));
+let b=parseInt(prompt("Enter the value b"));
+let c=parseInt(prompt("Enter the value c"));
+//let result=checklargest(a,b,c);
+//console.log( ` ${a},${b},${c} largest:${result}`);
+
+
+if(a>=b && a>=c){
+    console.log(a);
+}
+else if(b>=a && b>=c){
+    console.log(b)
+}
+else{
+    console.log(C)
+}
+
+    
+
+
+
